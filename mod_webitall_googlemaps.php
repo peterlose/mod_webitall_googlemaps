cf49cd2bcc9ee121a293eba0942197de22b23a3e
Peter Lose <peter@webitall.dk>
2014-11-03 22:24:46 +0100

    Upload

---
+++ mod_webitall_googlemaps.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright	@copyright	Copyright (c) 2014 Webitall (www.webitall.dk). All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+// include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+
+$options = new JRegistry;
+//$options->set('autoload', $params->get('framework'));
+$google = new JGoogle($options);
+
+$map = $google->embed('maps');
+$map->setAutoload('jquery');
+$map->setZoom($params->get('zoom'));
+$map->setCenter($params->get('address'), $params->get('headline'));
+$map->setMaptype($params->get('maptype'));
+
+$map->echoHeader();
+
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+
+require(JModuleHelper::getLayoutPath('mod_webitall_googlemaps', $params->get('layout', 'default')));
---
+++ tmpl/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright	Copyright (c) 2014 Webitall (www.webitall.dk). All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+?>
+
+<div class="map">
+    <?php $map->echoBody(); ?>
+</div>
+
+<pre>
+
+    <?php var_dump($map); ?>
+
+</pre>
